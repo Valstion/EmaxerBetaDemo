098daccd4306292ef4679c9c8fbfc64339ec5346
Valstion <lordvalstion@gmail.com>
2023-05-22 15:05:49 +0300

    Ну ещё добавили 2 строчки

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
+        System.out.println("Hello and welcome!");
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
             System.out.println("Первая проба пера так сказать");
             System.out.println("labas rytas");
         }
+
+
+
+
     }
 }

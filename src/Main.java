9b3806be255bda316e620eb8c0f6be1d86a0c31c
Valstion <lordvalstion@gmail.com>
2023-05-22 15:58:36 +0300

    Учимся считать.

--- src/Main.java
+++ src/Main.java
@@ -7,8 +7,30 @@ public class Main {
             System.out.println("labas rytas");
         }
 
-
-
+        int currentYear = 2023;
+        currentYear = 2024;
+        long future = 9877986451254165l;
+        float partical = 652456.78264587f;
+        float v = partical * 2;
+        System.out.println(v);
+        double partica2 = 652456.78264587f;
+        double z = partica2 / 2;
+        System.out.println(z);
+        boolean isSunny = false;
+        System.out.println(isSunny);
+        char letter = 'a';
+        char number = '1';
+        System.out.println(number + letter);
+        String name = "Jonas";
+        System.out.println(12 + 10 + " yra grazus skaicius");
+        System.out.println("grazus skaicius yra " + (10 + 2));
+        System.out.println("grazus skaicius yra " + 10 + 2);
+        System.out.println(5-5);
+        System.out.println(5+5);
+        System.out.println(5*5);
+        System.out.println(5==5);
+        System.out.println(12 % 5);
+        System.out.println(13 % 2 == 0);
 
     }
 }
